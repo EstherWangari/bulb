fade6f87177b2e8807c89986fb8b348499873963
Something Inc <KareWangari>
2019-04-09 16:13:06 +0300

    made it dynamic the images and the background

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,25 @@
-document.getElementById().src='lights-on.jpeg'=src.='lights-off.jpeg'
+function lightsOn(){
+  document.getElementById('bulb').src='./images/animated-light-bulb-image-0022.gif';
+  document.getElementById('body').style.background='#F9E739';
+  document.getElementById('header').style.display='block';
+  document.getElementById('three').style.display='none';
+  document.getElementById('socketon').style.display='none';
+  document.getElementById('socketoff').style.display='block';
+
+
+}
+function lightsOff(){
+  document.getElementById('bulb').src='./images/eyes.jpg';
+  document.getElementById('body').style.background='black';
+  document.getElementById('header').style.display='none';
+  document.getElementById('three').style.display='block';
+  document.getElementById('socketoff').style.display='none';
+  document.getElementById('socketon').style.display='block';
+
+
+
+
+
+
+
+}
